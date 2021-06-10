d946276b0f6823f05668804ca6a4c6cb130620fd
Henrique Amaral <henrique.amaral@adidas.com>
2021-06-10 16:42:41 +0200

    Added task list to todo domain

--- src/main/java/com/henriq/todo/domain/Task.java
+++ src/main/java/com/henriq/todo/domain/Task.java
@@ -3,33 +3,25 @@ package com.henriq.todo.domain;
 import com.henriq.todo.validator.SelfValidator;
 import lombok.Getter;
 
-import java.util.List;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-
-public class Todo extends SelfValidator<Todo> {
+@Getter
+public class Task extends SelfValidator<Task> {
 
   @NotNull
-  @Getter
   private final Long id;
 
   @NotEmpty
-  @Getter
   private final String name;
 
-  @Getter
   private final String description;
 
-  @Getter
-  private final List<Task> tasks;
-
-  public Todo(Long id, String name, String description,
-              List<Task> tasks) {
+  public Task(Long id, String name, String description) {
     this.id = id;
     this.name = name;
     this.description = description;
-    this.tasks = tasks;
     validateSelf();
   }
+
 }
--- src/test/java/com/henriq/todo/usecase/CreateTodoTest.java
+++ src/test/java/com/henriq/todo/usecase/CreateTodoTest.java
@@ -4,33 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
 
+import com.henriq.todo.domain.Task;
 import com.henriq.todo.domain.Todo;
 import com.henriq.todo.gateway.TodoGateway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 @ExtendWith(MockitoExtension.class)
 public class CreateTodoTest {
 
+  @InjectMocks
   private CreateTodo createTodo;
 
   @Mock
   private TodoGateway todoGateway;
 
-
-  @BeforeEach
-  void setup() {
-    createTodo = new CreateTodo(todoGateway);
-  }
-
   @Test
   void shouldCreateTodoWithSuccess() {
 
-    final var todo = new Todo(1L, "new Todo", "Description");
+    final var task = new Task(1L, "new task", "do bla bla");
+    final var todo = new Todo(1L, "new Todo", "Description", List.of(task));
 
     createTodo.execute(todo);
 
@@ -40,12 +40,15 @@ public class CreateTodoTest {
   @Test
   void shouldFailCreateTodoWithIdSameId() {
 
-    final var todo = new Todo(1L, "new super Todo", "Description");
+    final var task = new Task(1L, "new task", "do bla bla");
+    final var todo = new Todo(1L, "new super Todo", "Description", List.of(task));
     doThrow(new RuntimeException("error")).when(todoGateway).create(todo);
 
     final var mandatoryFieldsException = assertThrows(RuntimeException.class,
                                                       () -> createTodo.execute(todo));
+
     assertEquals("error", mandatoryFieldsException.getMessage());
+    Mockito.verify(todoGateway).create(todo);
   }
 
 }
